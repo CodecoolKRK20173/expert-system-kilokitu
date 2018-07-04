f4b23255604b0b82cb8d71f5171fd88a78887ad2
Tomasz Łukaszyk <el.moue@gmail.com>
2018-07-04 20:20:04 +0200

    Add Fact class

---
+++ src/main/java/com/codecool/containers/Fact.java
@@ -0,0 +1,39 @@
+package com.codecool.containers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Fact {
+
+    private String id;
+    private String description;
+    private Map<String, Boolean> evals;
+
+    public Fact(String id, String description) {
+        this.id = id;
+        this.description = description;
+        this.evals = new HashMap<>();
+        this.evals.put("family", null);
+        this.evals.put("money", null);
+        this.evals.put("comfort", null);
+        this.evals.put("luxury", null);
+    }
+
+    public Set<String> getIdSet() {
+        return this.evals.keySet();
+    }
+
+    public void setFactValueById(String id, boolean value) {
+        this.evals.put(id, value);
+    }
+
+    public boolean getValueById(String id) {
+        return this.evals.get(id);
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+}
