c045d4298034d6d0ed58514fdeb628ba58ace1fa
Tomasz Łukaszyk <el.moue@gmail.com>
2018-07-05 18:52:10 +0200

    Add Question class

---
+++ src/main/java/com/codecool/containers/Question.java
@@ -0,0 +1,30 @@
+package com.codecool.containers;
+
+public class Question {
+
+    private String id;
+    private String question;
+    private Answer answer;
+
+    public Question(String id, String question, Answer answer) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getQuestion() {
+        return this.question;
+    }
+
+    public Answer getAnswer() {
+        return this.answer;
+    }
+
+    public boolean getEvaluatedAnswer(String input) {
+        return this.answer.evaluateAnswerByInput(input);
+    }
+}
